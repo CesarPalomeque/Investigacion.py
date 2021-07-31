8f6fb29f5b7ebc859d414bb7ea39d0941b0aa267
CesarPalomeque <85848931+CesarPalomeque@users.noreply.github.com>
2021-07-31 03:52:56 -0500

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -1,3 +1,93 @@
-class Menu:
-    input()
+class Menu_Principal:
+    def __init__(self):
+     print("1)calculadora")
+#      print("2) operaciones con numero")
+#      print("3)tratamientos de listas")
+#      print("4)Operaciones de cadenas")
+#      print("5)salir")
+# opc=int(input("Elige una opción[1....5]: ") )
+
+     
+class Calculadora:
+    def __init__(self, numero1, numero2):
+
+     def suma(self):
+         self.num1=numero1
+         self.num2=numero2
+         self.suma=numero1 
+         #print('1)la suma es')
+         def resta():
+          #print('2)la resta es')
+          def mutiplicacion():
+              #print('3)la multiplicacion es')
+              def división():
+               #print('4)la division es')
+               class calEstandar(Calculadora):
+                def __init__(self, numero1, numero2):
+                 super().__init__()
+    def mutiplicacion(): # aplicar polimorfismo
+        #print('5)la multiplicacion es')
+        def exponente():
+         #print('6)el exponente es')
+         def valorAbsoluto(numero):
+          #print('7)el valor absoluto es')
+
+
+          class calCientifica(Calculadora):
+           PI = 3.1416 
+    def __init__(self, numero1, numero2):
+     super().__init__(numero1,numero2)
+    def circunferencia(radio):
+        #print('8)la circuferencia es')
+        def areaCirculo(radio):
+         #print('9)El area es')
+         def areaCuadrado(lado):
+            #print('10)salir')
+    #opccal=int(input("Elige una opción: ") )       
+          
+          print("2) operaciones con numero")
+    class Basico:
+         def  numerosN(n):
+          def multiplo(numero):
+           def DivisoresNumero(numero):
+            def primo(numero):
+             def perfecto(numero):
+
+              class Intermedio(Basico):
+               def numerosN(n): # aplicar polimorfismo
+                def factorial(numero): # 
+                 def fibonacci(n):
+                  def primosGemelos(num1,num2):
+                   def  amigos(num1,num2):
+                      print("3)tratamientos de listas")
+                      class Lista(Intermedio):
+                       def __init__(self, lista):
+                        def  presentarLista():
+                         def  buscarLista(valor):
+                          def  listaFactorial():
+                           def listaPrimo():
+                            def listaNotas(listaNotasDicccionario):
+                             def insertarLista(valor,posicion):
+  
+                              def eliminarLista(valor):
+                               def retornaValorLista(posicion):
+                                def copiarTuplaLista(tupla):
+                                 def vueltoLista(listaClientesDiccionario):
+                                  print("4)Operaciones de cadenas")
+                                  class Cadena:
+                                   def __init__(self, cadena):
+                                    def  recorrerCadena():
+                                     def  buscarCaracter(buscado):
+                                      def  listaPosiciones(caracter):
+                                       def listaPalabras():
+                                        def cadenaLista():
+                                         def insertarDato(buscado,posicion):
+                                          def eliminarOcurrencias(buscado):
+                                           def retornaValor(posicion):
+                                            def concatenarCadena(dato):
+                                             print("5)salir")
+opc=int(input("Elige una opción[1....5]: ") )
+
+Menu_Principal()
+
     
